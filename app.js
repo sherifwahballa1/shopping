7d9d46500cbf56e146ebd84d46567ff690a779d6
sherifwahballa1 <sherifwahballa0@gmail.com>
2021-10-06 10:03:11 +0200

    setup required packages and create running server app

---
+++ app.js
@@ -0,0 +1,46 @@
+const http = require("http");
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const xxs = require('xss-clean');
+
+const app = express();
+
+app.use(
+    cors({
+        credentials: true,
+        origin: ['http://localhost:9000', 'http://localhost:4200'],
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        allowedHeaders: [
+            'Content-Type',
+            'Authorization',
+            'origin',
+            'Access-Control-Allow-Headers',
+        ],
+        optionsSuccessStatus: 204,
+        preflightContinue: false,
+    }),
+)
+
+// secure Express app by setting various HTTP headers
+app.use(helmet());
+
+// parses incoming requests with JSON payloads and Controls the maximum request body size
+app.use(express.json({ limit: '10kb' }))
+app.use(express.urlencoded({ extended: true, limit: '10kb' }))
+
+// HTTP request logger middleware
+app.use(morgan('tiny'))
+
+// ====================== XXS =================================
+// Data sanitization against cross-site scripting (XSS)
+app.use(xxs()) // prevent if code contain html code or js code in body and convert it to symbols known
+
+
+
+const PORT = process.env.PORT || 9000;
+
+http.createServer(app).listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
